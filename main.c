83e85860de4620bd2dbeb2f05e5e29ac31b70463
John Mark Camota <103230518+Johnmarkcamota07@users.noreply.github.com>
2025-02-13 20:25:26 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "include/sarmap.h"
+
+int main() {
+    printf("Enter a string: ");
+    char *userInput = inputSTR();
+    clearBuffer();
+
+    if (userInput) {
+        printf("You entered: %s\n", userInput);
+    }
+
+    char **words = NULL;
+    words = arraySTR(userInput, words);
+
+    // Print stored words
+    for (int i = 0; words[i] != NULL; i++) {
+        printf("Word[%d]: %s\n", i, words[i]);
+    }
+
+    free(userInput);
+    freeArraySTR(words);
+
+    return 0;
+}
