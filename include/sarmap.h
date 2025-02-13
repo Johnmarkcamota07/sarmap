a574794ac4983180f9004041a39f5d1acc7db921
John Mark Camota <103230518+Johnmarkcamota07@users.noreply.github.com>
2025-02-13 21:37:35 +0800

    Update sarmap.h

--- include/sarmap.h
+++ include/sarmap.h
@@ -1,22 +1,13 @@
 #ifndef SARMAP_H
 #define SARMAP_H
 
-#ifdef _WIN32
-    #ifdef BUILD_SARMAP
-        #define SARMAP_EXPORT __declspec(dllexport)  // Export symbols when building DLL
-    #else
-        #define SARMAP_EXPORT __declspec(dllimport)  // Import symbols when using DLL
-    #endif
-#else
-    #define SARMAP_EXPORT   // No special directive needed for static/shared on Linux/macOS
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Function Declarations
-SARMAP_EXPORT char *inputSTR();
-SARMAP_EXPORT char **arraySTR(char *string, char **array);
-SARMAP_EXPORT void clearBuffer();
+// Function Declarations (No need for SARMAP_EXPORT since it's static-only)
+char *inputSTR();
+char **arraySTR(char *string, char **array);
+void clearBuffer();
+
 #endif // SARMAP_H
