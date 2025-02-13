3459247653d2c2b8f6355f226b7e5810172b88f0
John Mark Camota <103230518+Johnmarkcamota07@users.noreply.github.com>
2025-02-13 20:22:57 +0800

    Create sarmap.h

---
+++ include/sarmap.h
@@ -0,0 +1,14 @@
+#ifndef SARMAP_H
+#define SARMAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function Prototypes
+char *inputSTR();
+char **arraySTR(char *string, char **array);
+void clearBuffer();
+void freeArraySTR(char **array);
+
+#endif // END
