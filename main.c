a8668e003fe694c36a4c4618cecaf78afa209090
John Mark Camota <103230518+Johnmarkcamota07@users.noreply.github.com>
2025-02-13 21:27:28 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,39 @@
-#include "sarmap.h"
+#include "include/sarmap.h"
 
-int main() {
-    printf("Enter a string: ");
-    char *userInput = inputSTR();
-    clearBuffer();
+int main() //main function to check if everything is working
+{   
+    int count = 0;
+    char **array = NULL;
+    char choice;
 
-    if (userInput) {
-        printf("You entered: %s\n", userInput);
+    do {
+        printf("input string: ");
+        char *string = inputSTR();
+        array = arraySTR(string, array);
+        printf("string: %s\n", string);
+
+        if(array != NULL) {
+            count = 0;
+            while (array[count] != NULL) {
+                printf("string no. %d: %s\n", count, array[count]);
+                count++;
+            }
+        }
+        free(string); // Free the string after it has been added to the array
+        printf("Do you want to add another string? (y/n): ");
+        scanf(" %c", &choice);
+
+        clearBuffer();
+
+    } while(choice == 'y' || choice == 'Y');
+
+    if(array != NULL) {
+        count = 0;
+        while(array[count] != NULL) {
+            free(array[count]);
+            count++; // Increment count to avoid infinite loop
+        }
     }
-
-    char **words = NULL;
-    words = arraySTR(userInput, words);
-
-    // Print stored words
-    for (int i = 0; words[i] != NULL; i++) {
-        printf("Word[%d]: %s\n", i, words[i]);
-    }
-
-    free(userInput);
-
-    return 0;
+    free(array);
+    return 0; 
 }
